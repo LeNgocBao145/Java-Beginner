e98f0d99aebbd184be4a3793d362dab63f25c44a
LeNgocBao145 <himagito@gmail.com>
2024-07-06 19:37:43 +0700

    init project

---
+++ src/NewbieJav/HelloHCMUS.java
@@ -0,0 +1,8 @@
+package NewbieJav;
+
+public class HelloHCMUS {
+	public static void main(String[] args) {
+		System.out.printf("lol\n");
+		System.out.printf("Sum: ", 100);
+	}
+}
---
+++ src/NewbieJav/Lab1.java
@@ -0,0 +1,40 @@
+package NewbieJav;
+
+import java.util.Scanner;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// Exercise 1
+		System.out.println("Type name: ");
+		String name = scanner.nextLine();
+		System.out.println("Type score: ");
+		int score = scanner.nextInt();
+
+		System.out.println(name + " có điểm = " + score);
+
+		// Exercise 2
+		System.out.println("Chiều dài của hình chữ nhật là " + length);
+		double length = scanner.nextInt();
+
+		System.out.println("Chiều rộng của hình chữ nhật là " + width);
+		double width = scanner.nextInt();
+
+		double perimeter = (length + width) * 2;
+		double acreage = length * width;
+		double min = Math.min(length, width);
+
+		System.out.println("Chu vi hình chữ nhật là " + perimeter);
+		System.out.println("Diện tích hình chữ nhật là " + acreage);
+		System.out.println("Cạnh nhỏ nhất của hình chữ nhật là " + min);
+
+		// Exercise 3
+		System.out.println("Cạnh của khồi lập phương là ");
+		double edge = scanner.nextInt();
+
+		// double volume = edge * edge * edge;
+		double volume = Math.pow(edge, 3);
+
+		System.out.println("Thể tích hình lập phương là " + volume);
+	}
+}
